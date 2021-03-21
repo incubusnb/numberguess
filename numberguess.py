e269fb1fc0b215fef3552aa8257f020f65558a94
Parax <incubusnb@gmail.com>
2021-03-21 01:31:58 -0700

    Ask user to enter a guess

--- numberguess.py
+++ numberguess.py
@@ -1,4 +1,9 @@
 import random
 secretNumber = random.randint(1, 20)
 print('I am thinking of a number between 1 and 20.')
-print (secretNumber)
+
+
+for guessesTaken in range(1, 7):
+    print('Take a guess.')
+    guess = int(input())
+    
