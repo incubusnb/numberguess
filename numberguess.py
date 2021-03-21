a21e31868b597847cc35b929aeed3df8ccc72dbf
Parax <incubusnb@gmail.com>
2021-03-21 01:43:04 -0700

    Add conditions to inform player whether their guess was correct

--- numberguess.py
+++ numberguess.py
@@ -6,4 +6,11 @@ print('I am thinking of a number between 1 and 20.')
 for guessesTaken in range(1, 7):
     print('Take a guess.')
     guess = int(input())
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretnumber:
+        print('Your guess is too high.')
+    else:
+        break
     
